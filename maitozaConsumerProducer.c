7d38bffaa0b72464427420ebb903b41c59af1361
Joshua Maitoza <jmaitoza@umassd.edu>
2022-03-17 13:44:57 -0400

    started p3 skeleton

--- maitozaConsumerProducer.c
+++ maitozaConsumerProducer.c
@@ -1,1 +1,43 @@
-int main(int argc, char *argv[]) { return 0; }
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 8
+
+typedef struct {
+
+} item;
+
+int main(int argc, char *argv[]) {
+  item buffer[BUFFER_SIZE];
+  int in = 0;
+  int out = 0;
+
+  // Producer
+  item next_produced;
+  while (1) {
+    // produce an item in next_produced
+    while (((in + 1) % BUFFER_SIZE) == out)
+      ;
+    // buffer full: do nothing
+    buffer[in] = next_produced;
+    in = (in + 1) % BUFFER_SIZE;
+  }
+
+  // Consumer
+  item next_consumed;
+  while (1) {
+    while (in == out)
+      ;
+    // buffer empty: do nothing
+
+    next_consumed = buffer[out];
+    out = (out + 1) % BUFFER_SIZE;
+    // consume the item in next consumed
+  }
+
+  return 0;
+}

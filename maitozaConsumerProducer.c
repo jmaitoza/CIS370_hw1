95a93fd647322364110835a1cdd3a8bd4ad0acfe
joshm <jmaitoza@umassd.edu>
2022-03-23 19:13:05 -0400

    double cleanup

--- maitozaConsumerProducer.c
+++ maitozaConsumerProducer.c
@@ -86,27 +86,5 @@ int main(int argc, char *argv[]) {
       exit(0);
     }
   }
-  // Producer
-  // item next_produced;
-  // while (1) {
-  //   // produce an item in next_produced
-  //   while (((in + 1) % BUFFER_SIZE) == out)
-  //     ;
-  //   // buffer full: do nothing
-  //   buffer[in] = next_produced;
-  //   in = (in + 1) % BUFFER_SIZE;
-  // }
-
-  // // Consumer
-  // item next_consumed;
-  // while (1) {
-  //   while (in == out)
-  //     ;
-  //   // buffer empty: do nothing
-
-  //   next_consumed = buffer[out];
-  //   out = (out + 1) % BUFFER_SIZE;
-  // consume the item in next consumed
-
   return 0;
 }
